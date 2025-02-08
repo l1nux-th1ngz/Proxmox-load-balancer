fc20e541465d16d9291223015a77a1ae0d42ff03
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-02-08 09:27:35 -0700

    Update and rename requirements.txt to req.sh

---
+++ req.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt-get update
+
+# Install necessary Python packages
+sudo apt-get -y install python3-requests
+sudo apt-get -y install python3-urllib3
+sudo apt-get -y install python3-yaml
+sudo apt-get -y install python3-loguru
